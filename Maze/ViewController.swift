d729894d66f71fb0fb2cb2ec8e6edc389410a118
minutoni <minutoni105@gmail.com>
2019-04-11 00:33:29 +0900

    途中

--- Maze/ViewController.swift
+++ Maze/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //画面サイズの取得
+    let screenSize = UIScreen.main.bounds.size
+    
+    //迷路のマップを表した配列
+    let maze = [
+    [1,0,0,0,1,0],
+    [1,0,1,0,1,0],
+    [3,0,1,0,1,0],
+    [1,1,1,0,0,0],
+    [1,0,0,1,1,0],
+    [0,0,1,0,0,0],
+    [0,1,1,0,0,0],
+    [0,0,1,1,1,2],
+    ]
+    
+    //スタートとゴールを表すUIView
+    
+    var startView: UIView!
+    var goalView:UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func createView(x: Int,y:Int,width: CGFloat, height: CGFloat,offsetX:CGFloat,offsetY:CGFloat) -> UIView{
+        let rect = CGRect(x:0)
     }
 
 
